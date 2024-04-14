cb12ad7cd761c3813d7f4c4dd15d631e53724fcc
pranav page <pranavpage33@gmail.com>
2024-04-14 23:35:03 +0530

    Added tracing

--- src/navgest.py
+++ src/navgest.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
-import cv2, sched, time
+import cv2, sched, time, csv
 import mediapipe as mp 
 
 # Making a class to handle everything 
@@ -10,7 +10,7 @@ import mediapipe as mp
 # is processed by the model and the landmarks are output, or the landmarks from a stored .csv trace 
 # are output 
 class navgest:
-    def __init__(self, mode : str, save_trace : bool, interval : float):
+    def __init__(self, mode : str, save_trace : bool, interval : float, trace_file : str = None):
         self.mode = mode 
         self.save_trace = save_trace
         self.interval = interval
@@ -25,6 +25,9 @@ class navgest:
         self.state = np.empty(63)
         self.state[:] = np.nan
         self.last_state = self.state
+        self.diff = self.state
+        self.trace_file = trace_file
+
         self.landmark_indices = {
             'WRIST': 0,
             'THUMB_CMC': 1,
@@ -51,9 +54,31 @@ class navgest:
 
         if self.mode == "live":
             self.cap = cv2.VideoCapture(0)
+        elif self.mode == "playback":
+            self.trace_gen = self.trace_generator()
+        if self.save_trace:
+            self.make_trace_file()
         return
     
+    def make_trace_file(self):
+        with open(self.trace_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            headers = []
+            for name in self.landmark_indices.keys():
+                headers += [f'{name}_x', f'{name}_y', f'{name}_z']
+            headers += ['time']
+            writer.writerow(headers)
+        return
+    
+    def trace_generator(self):
+        data = pd.read_csv(self.trace_file)
+        for _, row in data.iterrows():
+            timestamp = row.get('timestamp', None)  # Fetch timestamp if available
+            landmarks_vector = row.drop('timestamp', errors='ignore').values
+            yield (timestamp, landmarks_vector)
+
     def start(self):
+        self.start_time = time.time()
         self.per_frame()
         self.scheduler.enterabs(time.time() + self.interval, 1, self.repeat, ())
         self.scheduler.run()
@@ -83,13 +108,27 @@ class navgest:
                             # print("Landmark vector:", landmark_vector)
                             self.last_state = self.state
                             self.state = landmark_vector
+                            self.diff = (self.state - self.last_state)/self.interval
+                            self.trace_state()
                             self.process_state()
+                            
 
                 cv2.imshow("Live Stream", frame)
 
             else:
                 print("FAIL")
+        elif(self.mode == "playback"):
+            # play the trace from the file 
+
+            pass
+            
         return
+    
+    def trace_state(self):
+        with open(self.trace_file, 'a', newline='') as file:
+            writer = csv.writer(file)
+            row = list(self.state) + [time.time()]
+            writer.writerow(row)
     
     def landmarks_to_vector(self, landmarks):
         if(landmarks):
@@ -109,18 +148,29 @@ class navgest:
     def __del__(self):
         self.stop()
 
+    def get_xy_from_vector(self, vector, landmark_name):
+        if landmark_name in self.landmark_indices:
+            base_index = 3 * self.landmark_indices[landmark_name]
+            x = vector[base_index]
+            y = vector[base_index + 1]
+            return (x, y)
+        else:
+            raise ValueError("Invalid landmark name provided.")
+
+
     def process_state(self):
         # for now, just print velocity
-        self.track_wrist()
+        self.track_points()
         return
     
-    def track_wrist(self):
-        diff = (self.state - self.last_state)/self.interval
-        print(f"Wrist vx, vy = {diff[:2]}")
+    def track_points(self):
+        print("Wrist vx, vy = ", self.get_xy_from_vector(self.diff, "WRIST"))
+        print("Index tip vx, vy = ", self.get_xy_from_vector(self.diff, "INDEX_FINGER_TIP"))
+        print("Pinky tip vx, vy = ", self.get_xy_from_vector(self.diff, "PINKY_TIP"))
         return
     
 def main():
-    nv = navgest("live", False, 0.1)
+    nv = navgest("live", True, 0.1, "./data/default.csv")
     nv.start()
 if __name__ == "__main__":
     main()
